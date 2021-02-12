3d589ee11725fddc5c7ea33bb3ba680f0cd4c8f0
Muhammad Adil <74587111+Adil-JK@users.noreply.github.com>
2021-02-12 19:00:39 +0500

    Update Inverse_kinematics_3dof.py

--- Inverse_kinematics_3dof.py
+++ Inverse_kinematics_3dof.py
@@ -11,22 +11,37 @@ a2 = 10.0
 a3 = 10.0
 
 # For Theta 1 (T1)
-T1 = np.arctan(y/x)
+if x==0 and y==0:
+    T1=0
+else:
+    T1 = np.arctan(y/x)
+    T1 = (T1*180.0)/np.pi
 
 # For Theta 2 (T2)
 r1 = np.sqrt(x**2 + y**2)
 r2 = z - a1
 phi2 = np.arctan(r2/r1)
+
+# phi2 in degrees
+phi2 = (phi2*180.0)/np.pi
 r3 = np.sqrt(r1**2 + r2**2)
+
 # For phi1, using law of cosines
 phi1 = np.arccos((a2**2+r3**2-a3**2)/(2*a2*r3))
+
+# phi1 in degrees
+phi1 = (phi1*180.0)/np.pi
 T2 = phi2 - phi1
+
 
 # For Theta 3
 # From law of cosines
 phi3 = np.arccos((a2**2+a3**2-r3**2)/(2*a2*a3))
+
+# phi3 in degrees
+phi3 = (phi3*180.0)/np.pi
 T3 = 180.0 - phi3
 
-print(T1)
-print(T2)
-print(T3)
+print("T1: ", T1)
+print("T2: ", T2)
+print("T3: ", T3)
